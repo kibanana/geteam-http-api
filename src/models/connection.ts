e7e4dd56f91d881e69e275bcbbe8ea9cbff5fb90
Kim Yewon <kyw017763@gmail.com>
2021-08-04 20:31:14 +0900

    Refactor models/*.ts

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,15 +1,17 @@
 import { connect } from 'mongoose'
-import config from '../../config'
+import config from '@config'
 
 export default connect(process.env.DB_URL || config.DB_URL, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  autoReconnect: true,
-  reconnectTries: Number.MAX_VALUE,
-}).then((connection) => {
-  console.log('Mongodb connected')
-  return connection
-}).catch((err) => {
-  console.log('Mongodb not connected')
-  console.log(err)
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    autoReconnect: true,
+    reconnectTries: Number.MAX_VALUE,
+})
+.then((connection) => {
+    console.log('Mongodb connected')
+    return connection
+})
+.catch((err) => {
+    console.log('Mongodb not connected')
+    console.log(err)
 })
